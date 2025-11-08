9b3f1099851e32f5fb65d065f3cbb648dc0b6cfc
Tarak Nath Jana <janatarak739@gmail.com>
2025-11-08 23:51:12 +0530

    change user password

--- apps/primary_backend/src/controller/auth.controller.ts
+++ apps/primary_backend/src/controller/auth.controller.ts
@@ -1,128 +1,200 @@
 import { async_handler } from "../utils/async_handler.js";
 import { api_error } from "../utils/api_error.js";
-import {api_responce }from "../utils/api_responce.js";
-import  {prisma } from "@master/database";
-import  bcrypt from "bcrypt";
-import  JWT from "jsonwebtoken"
+import { api_responce } from "../utils/api_responce.js";
+import { prisma } from "@master/database";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
 import { config } from "dotenv";
 
-config()
+config();
 
+// sign up controller
 
-// sign up controller 
+export const sign_up_controller = async_handler(async (req, res) => {
+  const { email, name, password } = req.body;
 
-export  const sign_up_controller = async_handler (async(req ,res )=>{
-    const { email , name , password} = req.body
+  //chack email and name field are exist or not
+  if ([email, name, password].some((field) => field?.trim() === "")) {
+    throw new api_error(400, "all field are required ");
+  }
 
-    //chack email and name field are exist or not
-    if (    
-        [email , name ,password].some((field)=> field?.trim() === "")
-    ){
-        throw  new api_error(400, "all field are required ")
-    }
+  // chack, this email are exit in db of exit then return responce : sorry this email are exist in database
+  const chack_email_in_out_database = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
 
+  if (chack_email_in_out_database) {
+    throw new api_error(
+      409,
+      "this email id are all rady exist try anather  email "
+    );
+  }
 
+  //  generate hash and chack hash are  generate or not
+  const saltRounds = bcrypt.genSaltSync(10);
 
-    // chack, this email are exit in db of exit then return responce : sorry this email are exist in database
-    const chack_email_in_out_database = await prisma.user.findFirst({
-        where:{
-            email:email
-        }
-    })
-    
-    if (chack_email_in_out_database){
-        throw new api_error (409 ,"this email id are all rady exist try anather  email ")
-    }
-   
+  const hash_password = bcrypt.hashSync(password, saltRounds);
 
-    //  generate hash and chack hash are  generate or not
-    const saltRounds =  bcrypt.genSaltSync(10 );
-    console.log("chack point : 2.5")
-    const hash_password =bcrypt.hashSync(password , saltRounds)
+  if (!hash_password) {
+    return new api_error(400, "password hashing are failed , try again lata");
+  }
 
-    if (!hash_password){
-        return new api_error(400,"password hashing are failed , try again lata")
-    }
+  // add new entry in our  db
 
+  const add_new_entry_in_our_db = await prisma.user.create({
+    data: {
+      email: email,
+      name: name,
+      password: hash_password,
+      is_active: true,
+      create_at: new Date(),
+    },
+    select: {
+      name,
+      email,
+    },
+  });
 
-    // add new entry in our  db 
-    
-    const add_new_entry_in_our_db = await prisma.user.create({
-        data:{
-            email:email,
-            name:name,
-            password:hash_password,
-            is_active:true,
-            create_at: new Date()
-        },
-        select:{
-            name,
-            email
-        }
-    })
+  if (!add_new_entry_in_our_db) {
+    throw new api_error(400, "database entry  failed  try again");
+  }
 
-   
-    if(!add_new_entry_in_our_db){
-        throw new api_error(400, "database entry  failed  try again")
-    }
+  return res
+    .status(201)
+    .json(
+      new api_responce(201, add_new_entry_in_our_db, " sign up success fully ")
+    );
+});
 
-    return res.status(201).json( new api_responce(201,add_new_entry_in_our_db," sign up success fully "))
-})
+// login controller
 
+export const login_controller = async_handler(async (req, res) => {
+  const { email, password } = req.body;
 
-// login controller 
+  // chack required fields
+  // chack user email id  are exit or not
+  // then chack password
+  // create access token
+  // return responce
+  if ([email, password].some((field) => field.trim() === "")) {
+    throw new api_error(400, "all field are required");
+  }
 
-export  const  login_controller = async_handler (async (req ,res)=>{
-    const  { email , password} = req.body;
+  const chack_email_in_out_db = await prisma.user.findFirst({
+    where: {
+      email: email,
+      is_active: true,
+    },
+    select: {
+      password: true,
+      email: true,
+    },
+  });
+  if (!chack_email_in_out_db) {
+    throw new api_error(404, "user not found ");
+  }
 
-    // chack required fields 
-    // chack user email id  are exit or not 
-    // then chack password 
-    // create access token 
-    // return responce
-    if ([email , password].some((field)=>field.trim() === "")){
-        throw  new api_error(400,"all field are required")
-    }
+  const matching_password_and_hash = bcrypt.compareSync(
+    password,
+    chack_email_in_out_db.password
+  );
+  if (!matching_password_and_hash) {
+    throw new api_error(401, "Invalid credentials. Password does not match");
+  }
+  const token = {
+    email: chack_email_in_out_db.email,
+  };
 
-    const chack_email_in_out_db = await prisma.user.findFirst({
-        where:{
-            email:email,
-            is_active:true
-        },
-        select:{
-            password :true,
-            email:true
-        }
-    })
-    if (!chack_email_in_out_db){
-        throw new api_error(404,"user not found ")
-    }
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new api_error(400, " env not exist ");
+  }
+  const access_token = JWT.sign(token, secret);
+  if (!access_token) {
+    throw new api_error(400, "  failed to create  access token  ");
+  }
 
-
-
-    const matching_password_and_hash  =bcrypt.compareSync(password ,chack_email_in_out_db.password)
-    if (!matching_password_and_hash) {
-        throw new api_error(401,"Invalid credentials. Password does not match");     
-    }
-    const token = {
-         email: chack_email_in_out_db.email,
-    }
-
-
-    const secret = process.env.JWT_SECRET
-    if(!secret){
-        throw new api_error(400," env not exist "); 
-    }
-    const access_token = JWT.sign(token, secret)
-    if (!access_token){
-        throw new api_error(400,"  failed to create  access token  ");   
-    }
-
-    return res.status(200).json(
-        new api_responce(200,{
-            access_token,
-            email 
-        }," login success fully")
+  return res.status(200).json(
+    new api_responce(
+      200,
+      {
+        access_token,
+        email,
+      },
+      " login success fully"
     )
+  );
+});
 
-})
+// forgot password
+export const change_user_password = async_handler(async (req, res) => {
+  // chack fields
+  // chack email are exist or not then compare this  password
+  // if  password match  then hash new password and update on our database
+  // return responce
+  const { email, old_password, new_password } = req.body;
+
+  if (
+    [email, old_password, new_password].some((field) => field.trim() === "")
+  ) {
+    throw new api_error(400, "all field are required ");
+  }
+
+  const chack_email_in_out_db = await prisma.user.findFirst({
+    where: {
+      email: email,
+      is_active: true,
+    },
+    select: {
+      password: true,
+    },
+  });
+  if (!chack_email_in_out_db) {
+    throw new api_error(404, "this email are not  exit ");
+  }
+
+  const matching_password_and_hash = bcrypt.compareSync(
+    old_password,
+    chack_email_in_out_db.password
+  );
+  if (!matching_password_and_hash) {
+    throw new api_error(401, "Invalid credentials. Password does not match");
+  }
+
+  //  generate hash and chack hash are  generate or not
+  const saltRounds = bcrypt.genSaltSync(10);
+
+  const hash_password = bcrypt.hashSync(new_password, saltRounds);
+
+  if (!hash_password) {
+    return new api_error(400, "password hashing are failed , try again lata");
+  }
+
+  const update_user_password = await prisma.user.update({
+    where: {
+      email: email,
+    },
+    data: {
+      password: hash_password,
+    },
+    select: {
+      email: true,
+    },
+  });
+
+  if (!update_user_password) {
+    throw new api_error(400, "database entry  failed  try again");
+  }
+
+  return res
+    .status(201)
+    .json(
+      new api_responce(
+        201,
+        { data: update_user_password },
+        "success fully update your password"
+      )
+    );
+});
--- apps/primary_backend/src/router/index.ts
+++ apps/primary_backend/src/router/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { sign_up_controller,login_controller } from "../controller/auth.controller.js";
+import { sign_up_controller,login_controller ,change_user_password } from "../controller/auth.controller.js";
 
 
 const routers = express.Router();
 
 routers.route("/signup").post(sign_up_controller);
 routers.route("/login").post(login_controller);
+routers.route("/update-password").post(change_user_password);
 
 export default  routers
 
