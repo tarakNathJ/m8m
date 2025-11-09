388e3429174abf9f53c0fe11d62dcf4ff1d51d8c
Tarak Nath Jana <janatarak739@gmail.com>
2025-11-09 19:57:08 +0530

    minor changes

--- apps/primary_backend/src/app.ts
+++ apps/primary_backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import type{ Express} from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import routers from "./router/index.js";
+import routers from "./router/index.route.js";
 
 
 
