bb8e2122e3658292b115e7e54fa454f38b73bc2f
Tarak Nath Jana <janatarak739@gmail.com>
2025-11-13 00:00:42 +0530

    create a class thay send email (node) for worker backend

---
+++ apps/worker/src/apps/gmail.apps.ts
@@ -0,0 +1,67 @@
+import { createTransport } from "nodemailer";
+import  type{ gmail_type } from "../types/index.js";
+import { prisma ,schemaType } from "@master/database";
+
+class mail_sender {
+  private transport;
+
+  constructor(
+    sender_email: gmail_type["sender_email"],
+    sender_app_pass: gmail_type["sender_app_pass"],
+    recever_email: gmail_type["recever_email"],
+    message: gmail_type["message"],
+    subject: gmail_type["subject"],
+    stepes_run_id:number,
+    index:number
+  ){
+    this.transport  = createTransport ({
+        service:"gmail",
+        auth:{
+            user:sender_email,
+            pass:sender_app_pass
+        }
+        
+    })
+    this.transport.sendMail({
+        from:sender_email,
+        to:recever_email,
+        subject:subject,
+        text:message
+    },async (err , info)=>{
+        if(err){
+            console.error(err);
+            process.exit(1);
+            
+        }else{
+            console.log(info);
+
+            const save_message_id = await prisma.working_step_validator.create({
+                data:{
+                    
+                    message_id:info.messageId,
+                    stepes_run_id:stepes_run_id,
+                    email:recever_email,
+                    status:schemaType.step_validation_status.SUCCCESS,
+                    create_at: new Date(),
+                    index:index
+                }
+            })
+            if (!save_message_id){
+                console.log("db error")
+
+                throw new Error
+            }
+
+            // info.messageId
+            return true
+        }
+
+    })
+  };
+
+
+
+}
+
+
+export {mail_sender}
---
+++ apps/worker/src/types/index.ts
@@ -0,0 +1,8 @@
+export type gmail_type = {
+
+    sender_app_pass : string,
+    sender_email : string,
+    recever_email : string,
+    message : string,
+    subject : string
+}
