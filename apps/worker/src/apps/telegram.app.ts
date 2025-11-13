f7fc77ad4a3aaa2986262bff75b0c6505ab0015c
Tarak Nath Jana <janatarak739@gmail.com>
2025-11-13 16:45:20 +0530

    write a new private function  for send_message_on_telegram_bot in worker backend

--- apps/worker/src/apps/telegram.app.ts
+++ apps/worker/src/apps/telegram.app.ts
@@ -1,9 +1,10 @@
 import { config } from "dotenv";
-import { prisma } from "@master/database";
+import { prisma, schemaType } from "@master/database";
 import TelegramBot, { type ConstructorOptions } from "node-telegram-bot-api";
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
-import {aws_s3_service_provider}  from "../utils/aws_s3.js"
+import { aws_s3_service_provider } from "../utils/aws_s3.js";
+import { get } from "http";
 
 config();
 
@@ -27,7 +28,6 @@ class send_message_on_telegram_bot {
         throw new Error("telegram message failed");
       }
 
-
       return true;
     } catch (error: any) {
       console.log(error.message);
@@ -35,35 +35,116 @@ class send_message_on_telegram_bot {
     }
   }
 
-  private async send_file_to_telegram( token: string, chat_id: number, messages: object) {
+  private async send_file_to_telegram(
+    token: string,
+    chat_id: number,
+    messages: object
+  ) {
     try {
-        
-        const {message :caption }  = messages as {message: string, file_name: string};
-        const file_name = await new aws_s3_service_provider().download_file((messages as {file_name: string}).file_name);
+      const { message: caption } = messages as {
+        message: string;
+        file_name: string;
+      };
+      const file_name = await new aws_s3_service_provider().download_file(
+        (messages as { file_name: string }).file_name
+      );
 
-        if (!file_name || typeof(file_name) !== "string" || !fs.existsSync(file_name) ){
-            return false
-        }
+      if (
+        !file_name ||
+        typeof file_name !== "string" ||
+        !fs.existsSync(file_name)
+      ) {
+        return false;
+      }
 
-        const requestOption = {
-            agentOptions: {
-                family: 4,
+      const requestOption = {
+        agentOptions: {
+          family: 4,
+        },
+      };
+
+      const bot = new TelegramBot(token, requestOption as ConstructorOptions);
+      const fileStream = fs.createReadStream(path.resolve(file_name));
+      await bot.sendDocument(chat_id, fileStream, {
+        caption,
+        // @ts-ignore
+        contentType: "application/octet-stream",
+      });
+
+      fs.unlink(file_name, () => {
+        console.log("file delete success fully");
+      });
+      return true;
+    } catch (error: any) {
+      console.log(error.message);
+      throw new Error(error.message);
+    }
+  }
+
+  private async send_email_template(
+    token: string,
+    chat_id: number,
+    message: object
+  ) {
+    try {
+      // fetch email template  for db
+      const get_email_template = await prisma.mail_template.findUnique({
+        where: {
+          // @ts-ignore
+          reseve_email_validator_id: message.resever_email_validator_id,
+        },
+        select: {
+          template: true,
+        },
+      });
+
+      if (!get_email_template) {
+        console.log("email template not found");
+        throw new Error("email template not found");
+      }
+
+      const status_for_send_message = await this.send_message(
+        token,
+        chat_id,
+        get_email_template.template as object
+      );
+      if (!status_for_send_message) {
+        console.log("email template send failed");
+        throw new Error("email template send failed");
+      }
+
+      const complete_email_template_validation = await prisma.$transaction(
+        async (ts) => {
+          const reseve_email_validator_id_status_update =
+            await ts.reseve_email_validator.update({
+              where: {
+                // @ts-ignore
+                id: message.resever_email_validator_id,
+              },
+              data: {
+                // @ts-ignore
+                status: schemaType.step_validation_status.DONE,
+                update_at: new Date(),
+              },
+              select: {
+                working_step_validator_id: true,
+              },
+            });
+          await ts.working_step_validator.update({
+            where: {
+              id: reseve_email_validator_id_status_update.working_step_validator_id,
             },
-        };
-        
-        const bot = new TelegramBot(token, requestOption as ConstructorOptions);
-        const fileStream = fs.createReadStream(path.resolve(file_name));
-        await bot.sendDocument(chat_id, fileStream,{
-            caption,
-            // @ts-ignore
-            contentType: "application/octet-stream",
+            data: {
+              status: schemaType.step_validation_status.DONE,
+              update_at: new Date(),
+            },
+          });
 
-        })
+          return true;
+        }
+      );
 
-    fs.unlink(file_name, () => {
-      console.log("file delete success fully");
-    });
-    return true
+      return complete_email_template_validation ? true : false;
     } catch (error: any) {
       console.log(error.message);
       throw new Error(error.message);
@@ -75,15 +156,24 @@ class send_message_on_telegram_bot {
     chat_id: number,
     message: object
   ) {
-    switch (message) {
-      case ("file_name" in message) as unknown as object:
-        await this.send_file_to_telegram(token, chat_id, message);
-        break;
-      case ("resever_email_datas" in message) as unknown as object:
-        break;
-      default:
-        await this.send_message(token, chat_id, message);
-        break;
+    try {
+      let status = false;
+      switch (message) {
+        case ("file_name" in message) as unknown as object:
+          status = await this.send_file_to_telegram(token, chat_id, message);
+          break;
+        case ("resever_email_datas" in message) as unknown as object:
+          status = await this.send_email_template(token, chat_id, message);
+          break;
+        default:
+          status = await this.send_message(token, chat_id, message);
+          break;
+      }
+
+      return status;
+    } catch (error: any) {
+      console.log(error.message);
+      throw new Error(error.message);
     }
   }
 }
